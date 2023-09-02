b3b639cea86996fd99aec255169f7ac85c20b887
KBeKind <79383539+KBeKind@users.noreply.github.com>
2023-09-02 10:12:55 -0500

    got a working 3d cube rendered and rotating  added edge colors and inner color with transparency

--- js/main.js
+++ js/main.js
@@ -12,15 +12,33 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+const geometry = new THREE.BoxGeometry(2, 2, 2);
+const material = new THREE.MeshBasicMaterial({
+  color: 0xffca3a,
+  wireframe: true,
+});
 const cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
+
+const geometry2 = new THREE.BoxGeometry(2, 2, 2);
+const material2 = new THREE.MeshBasicMaterial({
+  color: 0x6a4c93,
+  transparent: true,
+  opacity: 0.5,
+});
+const cube2 = new THREE.Mesh(geometry2, material2);
+scene.add(cube2);
 
 camera.position.z = 5;
 
 function animate() {
   requestAnimationFrame(animate);
+
+  cube.rotation.x += 0.01;
+  cube.rotation.y += 0.01;
+  cube2.rotation.x += 0.01;
+  cube2.rotation.y += 0.01;
+
   renderer.render(scene, camera);
 }
 animate();
